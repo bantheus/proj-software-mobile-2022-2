33ed9156bc7c9abc2f279b636cb188d2d272e765
Matheus Schmidt <matheuschmidt.b@gmail.com>
2022-09-27 20:23:57 -0300

    fixed

--- android/app/src/main/kotlin/com/example/miaudote/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.miaudote
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:miaudote/my_app.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
