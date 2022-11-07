cae8214b8b568b07be73cbaa5cca20cf1f32fe7e
unknown <pabsan@alunos.utfpr.edu.br>
2022-11-06 22:48:38 -0300

    perfil changes

--- lib/screens/perfil_screen.dart
+++ lib/screens/perfil_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:miaudote/models/users.dart';
 //import 'package:miaudote/models/user.dart';
 import 'package:miaudote/repositories/user_repository.dart';
@@ -25,6 +28,62 @@ class PerfilPage extends StatefulWidget {
 }
 
 class _PerfilPageState extends State<PerfilPage> {
+  XFile? imageFile;
+
+  _abrirGaleria() async {
+    final picker = ImagePicker();
+
+    try {
+      XFile? file = await picker.pickImage(source: ImageSource.gallery);
+      if (file != null) setState(() => imageFile = file);
+      
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _abrirCamera() async {
+    
+    final picker = ImagePicker();
+
+    try {
+      XFile? file = await picker.pickImage(source: ImageSource.camera);
+      if (file != null) setState(() => imageFile = file);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _escolha(BuildContext context) {
+    final usuario = Provider.of<UserRepository>(context, listen: false);
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Escolha"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text("Galeria"),
+                    onTap: () {
+                      _abrirGaleria();
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.all(8)),
+                  GestureDetector(
+                    child: Text("Câmera"),
+                    onTap: () {
+                      _abrirCamera();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,11 +114,49 @@ class _PerfilPageState extends State<PerfilPage> {
                 child: SizedBox(
                   width: 250,
                   height: 150,
-                  child: Image.asset((widget.usuario.user?.foto == null
-                      ? "teste"
-                      : widget.usuario.user!.foto)),
-                ),
+                //   child: Image.asset((widget.usuario.user?.foto == null
+                //       ? "teste"
+                //       : widget.usuario.user!.foto)),
+                // ),
+                child: Stack(
+                      clipBehavior: Clip.none,
+                      fit: StackFit.expand,
+                      children: [
+                        CircleAvatar(
+                          child: ClipOval(
+                              child: imageFile != null
+                                  ? Image.file(File(imageFile!.path))
+                                  : null),
+                          radius: 10,
+                        ),
+                        Positioned(
+                          right: 30,
+                          bottom: 0,
+                          child: SizedBox(
+                            height: 46,
+                            width: 46,
+                             child: TextButton (
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                  side: BorderSide(color: Colors.white),
+                                ),
+                              ),
+                              onPressed: () {
+                                _escolha(context);
+                              },
+                              child: Icon(Icons.camera_alt_outlined),
+                             
+                          ),
+                        )
+                        )
+                      ],
+                    )),
+
+
               ),
+              
               Padding(
                   padding: const EdgeInsets.symmetric(
                     vertical: 20,
